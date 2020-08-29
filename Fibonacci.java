6766d72d013b89b8485209faafe016ddb25d62ac
akkir25 <69202365+akkir25@users.noreply.github.com>
2020-08-29 19:35:53 +0530

    Fibonacci.java is used to print Fibonacci series till 10 terms

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci
 
         for (int i = 1; i <= n; ++i)
         {
-            System.out.print(t1 + " + ");
+            System.out.print(t1 + "  ");
 
             int sum = t1 + t2;
             t1 = t2;
